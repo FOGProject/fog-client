2e998e505e63aa49a666265258c2b3f894ad0ee4
Jbob <contact@jbob.io>
2014-12-21 15:45:07 -0600

    Fix build dependencies

--- AESTest/Program.cs
+++ AESTest/Program.cs
@@ -1,37 +1,24 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Joseph
- * Date: 12/19/2014
- * Time: 1:43 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
-namespace FOG
-{
-	class Program
-	{
-		public static void Main(string[] args)
-		{
+namespace FOG {
+	class Program {
+		public static void Main(string[] args) {
 			
-
 			LogHandler.setConsoleMode(true);
 			CommunicationHandler.getAndSetServerAddress();
-
 			
 			Console.WriteLine("Beginning auth test");
 			Console.WriteLine();
-			
 			LogHandler.divider();
+			
 			for(int i = 0; i<= 0; i++) {
 				CommunicationHandler.authenticate();
 			}
+			
 			Console.WriteLine();
 			LogHandler.divider();
 			Console.Write("Press any key exit...");
 			Console.ReadKey(true);			
-
 		}
 	}
 }
