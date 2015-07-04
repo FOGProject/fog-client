fd94f4bde064010700c5cfac85bae2506ef7e36f
Jbob <contact@jbob.io>
2015-07-03 20:30:16 -0500

    Set default company to FOG

--- SignBinaries.cmd
+++ SignBinaries.cmd
@@ -7,10 +7,10 @@ START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\PipeServer.dll
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGService.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGUpdateHelper.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGUpdateWaiter.exe
+START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGNotificationGUI.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGUserService.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGTray.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\Debugger.exe
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\Microsoft.Win32.TaskScheduler.dll
-START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\System.Threading.Tasks.NET35.dll
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\SetupHelper.dll
 START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\SetupHelper.CA.dll
---
+++ SignInstaller.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+START /wait /B cmd /c %~dp0SignCode.cmd %~dp0bin\FOGService.msi
