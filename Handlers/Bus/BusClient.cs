a30013543d4ebe136216c0abf9f435734e80c146
Jbob <contact@jbob.io>
2015-07-05 17:17:21 -0500

    Make socket client

---
+++ Handlers/Bus/BusClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using WebSocket4Net;
+
+namespace FOG.Handlers
+{
+    class BusClient
+    {
+        public WebSocket Socket { get; private set; }
+        private const string LogName = "Bus::Client";
+
+        public BusClient(int port)
+        {
+            Socket = new WebSocket("ws://localhost:" + port + "/");
+        }
+
+        public void Start()
+        {
+            Socket.Open();
+        }
+
+        public void Stop()
+        {
+            Socket.Close();
+            Socket.Dispose();
+        }
+
+        public void Send(string message)
+        {
+            try
+            {
+                Socket.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(LogName, "Could not send message");
+                Log.Error(LogName, ex);
+            }
+        }
+    }
+}
---
+++ Handlers/Bus/BusServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SuperSocket.SocketBase;
+
+namespace FOG.Handlers
+{
+    class BusServer
+    {
+        public AppServer Socket { get; private set; }
+        private const string LogName = "Bus::Server";
+
+        public BusServer(int port)
+        {
+            Socket = new AppServer();
+
+            if (!Socket.Setup(port))
+               Log.Error(LogName, "Could not start server on port " + port);
+        }
+
+        public bool Start()
+        {
+            return Socket.Start();
+        }
+
+        public void Stop()
+        {
+            Socket.Stop();
+            Socket.Dispose();
+        }
+
+        public void Send(string message)
+        {
+            foreach (var session in Socket.GetAllSessions())
+                try
+                {
+                    session.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(LogName, "Could not send message");
+                    Log.Error(LogName, ex);
+                }
+        }
+    }
+}
