833bfe4f2d8e925ad074edc58dd63282e26b3131
Jbob <contact@jbob.io>
2015-07-10 01:39:57 -0500

    Use http for tests

--- FOGService.Tests/Handlers/CommunicationTests.cs
+++ FOGService.Tests/Handlers/CommunicationTests.cs
@@ -10,7 +10,7 @@ namespace FOGService.Tests.Handlers
     [TestFixture]
     public class CommunicationTests
     {
-        private const string Server = "https://fog.jbob.io/fog";
+        private const string Server = "http://fog.jbob.io/fog";
         private const string MAC = "1a:2b:3c:4d:5e:6f";
         private const string URL = "/service/Test.php?unit=";
         private const string PassKeyHex = "66733579595144305635386865727967316e746236395a6c6d48355a39313863";
