f7bbe2e3522374fcd0b0a5fdf463230d691ed6d4
Jbob <contact@jbob.io>
2015-07-14 17:54:15 -0500

    Updated dependency checks for nix

--- NixInstaller/install.sh
+++ NixInstaller/install.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-die () {
-    echo >&2 "$@"
-    exit 1
-}
-[ "$#" -eq 1 ] || die "1 argument required, $# provided"
-
-protocol=`echo $1 | awk -F: '{print $1;}'`
-domain=`echo $1 | awk -F/ '{print $3;}'`
-webroot=`echo $1`
-
-echo "Protocol: $protocol"
-echo "Domain:   $domain"
-echo "WebRoot:  $webroot"
+bash ./scripts/dependency.sh
+echo "Configuring..."
--- NixInstaller/scritps/dependency.sh
+++ NixInstaller/scritps/dependency.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+# Make a list of critical vs optional dependencies
+critical_commands=(mono mono-service unzip wget);
+optional_commands=(xprintidle);
+
+echo "Checking required dependencies..."
+
+for i in ${critical_commands[@]}; do
+        if [ hash $i 2>/dev/null ]; then
+			printf "%-20s %-30s" $i "Failed";
+			exit 1;
+		else
+			printf "%-20s %-30s" $i "Success";
+		fi
+		echo "";
+done
+
+echo ""	
+echo "Checking optional dependencies..."
+for i in ${optional_commands[@]}; do
+        if [ hash $i 2>/dev/null ]; then
+			printf "%-20s %-30s" $i "Failed";
+		else
+			printf "%-20s %-30s" $i "Success";
+		fi
+		echo "";
+done
