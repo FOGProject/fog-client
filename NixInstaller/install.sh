36ff2fb59438e0caed1c596dea8ecf3c4ef939d7
Jbob <contact@jbob.io>
2015-07-14 00:40:17 -0500

    Add basic url extraction to installer

--- NixInstaller/install.sh
+++ NixInstaller/install.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+protocol=`echo $1 | awk -F: '{print $1;}'`
+domain=`echo $1 | awk -F/ '{print $3;}'`
+webroot=`echo $1`
+
+echo "Protocol: $protocol"
+echo "Domain:   $domain"
+echo "WebRoot:  $webroot"
