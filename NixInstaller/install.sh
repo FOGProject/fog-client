22ba1a51d4f646a6019007913e5331d23ca7b0cc
Jbob <contact@jbob.io>
2015-07-14 21:51:18 -0500

    Add linux init.d script

--- NixInstaller/install.sh
+++ NixInstaller/install.sh
@@ -3,15 +3,23 @@ die () {
     echo >&2 "$@"
     exit 1
 }
+
+########################
+# Check Dependencies
+########################
+
+if [ "$(id -u)" != "0" ]; then
+   die "This script must be run as root"
+fi
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
-critical_commands=(mono mono-service unzip wget);
+critical_commands=(mono mono-service unzip curl);
 optional_commands=(xprintidle);
 
 echo "Checking required dependencies..."
 
 for i in ${critical_commands[@]}; do
-        if [ hash $i 2>/dev/null ]; then
+        if [ '$(hash $i)' != '' ]; then
 			printf "%-20s %-30s" $i "Failed";
 			exit 1;
 		else
@@ -23,7 +31,7 @@ done
 echo ""	
 echo "Checking optional dependencies..."
 for i in ${optional_commands[@]}; do
-        if [ hash $i 2>/dev/null ]; then
+        if [ '$(hash $i)' != ''  ]; then
 			printf "%-20s %-30s" $i "Failed";
 		else
 			printf "%-20s %-30s" $i "Success";
@@ -31,3 +39,24 @@ for i in ${optional_commands[@]}; do
 		echo "";
 done
 
+########################
+# Perform Installation
+########################
+
+echo ""
+echo "Downloading files..."
+curl -o /opt/FOGService.zip $1client/download.php?newclientzip
+echo "Extracting files..."
+unzip /opt/FOGService.zip /opt/fog-service
+echo "Adjusting permissions..."
+touch /opt/fog-service/fog.log
+chmod 775 /opt/fog-service/fog.log
+
+mono /opt/fog-service/SetupHelper.exe $1
+
+########################
+# Clean Up
+########################
+
+rm /opt/FOGService.zip
+rm /opt/fog-service/SetupHelper.exe
