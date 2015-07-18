65adf2f7ce9eb571556fa823d6a211405e9bad53
Jbob <contact@jbob.io>
2015-07-18 12:54:20 -0500

    Add User Core unit tests

---
+++ FOGService.Tests/Handlers/UserTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using FOG.Handlers;
+
+namespace FOGService.Tests.Handlers
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            Log.Output = Log.Mode.Console;
+        }
+
+        [Test]
+        public void IsUserLoggedIn()
+        {
+            Assert.IsTrue(UserHandler.IsUserLoggedIn());
+        }
+
+        [Test]
+        public void GetInactivityTime()
+        {
+            if(Settings.OS == Settings.OSType.Windows)
+                Assert.IsTrue(UserHandler.GetInactivityTime() != -1);
+            else
+                Assert.IsTrue(UserHandler.GetInactivityTime() == -1);
+        }
+
+        [Test]
+        public void GetCurrentUser()
+        {
+            Assert.AreEqual(Environment.UserName, UserHandler.GetCurrentUser());
+        }
+
+        [Test]
+        public void GetUsersLoggedIn()
+        {
+            var users = UserHandler.GetUsersLoggedIn();
+            Assert.IsTrue(users.Count >= 1);
+            Assert.IsTrue(users.Contains(Environment.UserName));
+        }
+
+    }
+}
