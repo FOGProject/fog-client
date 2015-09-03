a298cb2527399d46c67fc598e8d5f8b199a5d72d
Jbob <contact@jbob.io>
2015-09-02 20:19:56 -0500

    Close #15

--- Service/FOGSystemService.cs
+++ Service/FOGSystemService.cs
@@ -84,10 +84,12 @@ namespace FOG
         {
             var response = Communication.GetResponse("/management/index.php?node=client&sub=configure");
 
-            if (response.Error || response.IsFieldValid("#sleep")) return null;
+            if (response.Error) return null;
 
             // Set the shutdown graceperiod
             RegistryHandler.SetSystemSetting("gracePeriod", response.GetField("#promptTime"));
+
+            if (!response.IsFieldValid("#sleep")) return null;
 
             try
             {
