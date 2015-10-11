38361c765cdf32156bf8e3713a37bddad327a852
tomsealy <thomassealy@yahoo.com>
2015-10-11 10:01:54 -0400

    Add OS X files to Script directory
    This commit add the required files for OS X service start along with
    adding the commands to copy them

--- UniversalInstaller/Mac/MacInstall.cs
+++ UniversalInstaller/Mac/MacInstall.cs
@@ -1,17 +1,17 @@
 ﻿/*
  * FOG Service : A computer management client for the FOG Project
  * Copyright (C) 2014-2015 FOG Project
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -37,12 +37,14 @@ namespace FOG
             if (!File.Exists(logLocation))
                 File.Create(logLocation);
             ProcessHandler.Run("chmod", "755 " + logLocation);
-
-            ProcessHandler.Run("chmod", "755 /etc/init.d/FOGService");
-            ProcessHandler.Run("chmod", "755 /etc/init.d/FOGService");
-
-            ProcessHandler.Run("systemctl", "enable FOGService >/ dev / null 2 > &1");
-            ProcessHandler.Run("sysv-rc-conf", "FOGService on >/ dev / null 2 > &1");
+            Helper.ExtractResource("FOG.Scripts.fog.useragent", Path.Combine(GetLocation(), "fog.useragent"));
+            ProcessHandler.Run("chmod", "755 "+ Path.Combine(GetLocation(), "fog.useragent"));
+            Helper.ExtractResource("FOG.Scripts.fog.daemon", Path.Combine(GetLocation(), "fog.daemon"));
+            ProcessHandler.Run("chmod", "755 "+ Path.Combine(GetLocation(), "fog.daemon"));
+            Helper.ExtractResource("FOG.Scripts.com.freeghost.daemon.plist", "/Library/LaunchDaemons/com.freeghost.daemon.plist");
+            ProcessHandler.Run("chown", "root /Library/LaunchDaemons/com.freeghost.daemon.plist");
+            Helper.ExtractResource("FOG.Scripts.com.freeghost.useragent.plist", "/Library/LaunchAgents/com.freeghost.useragent.plist");
+            ProcessHandler.Run("chown", "root /Library/LaunchAgents/com.freeghost.useragent.plist");
             return true;
         }
 
@@ -53,15 +55,16 @@ namespace FOG
 
         public string GetLocation()
         {
-            return "/opt/fog-client";
+            return "/opt/fog-service";
         }
 
         public bool Uninstall()
         {
             Directory.Delete(GetLocation());
-            ProcessHandler.Run("systemctl", "disable FOGService >/ dev / null 2 > &1");
-            ProcessHandler.Run("sysv-rc-conf", "FOGService off >/ dev / null 2 > &1");
-            File.Delete("/etc/init.d/FOGService");
+            ProcessHandler.Run("launchctl", "unload -w /Library/LaunchDaemons/org.freeghost.daemon.plist");
+            ProcessHandler.Run("launchctl", "unload -w /Library/LaunchAgents/org.freeghost.useragent.plist");
+            File.Delete("/Library/LaunchAgents/com.freeghost.useragent.plist");
+            File.Delete("/Library/LaunchDaemons/com.freeghost.daemon.plist");
             return true;
         }
     }
