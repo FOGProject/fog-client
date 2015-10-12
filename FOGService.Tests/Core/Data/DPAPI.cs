44d8688f6d3d7298c475ca3afb4fef49ad016618
Jbob <contact@jbob.io>
2015-10-12 11:32:02 -0500

    Ensure DPAPI alters the bytes

--- FOGService.Tests/Core/Data/DPAPI.cs
+++ FOGService.Tests/Core/Data/DPAPI.cs
@@ -37,13 +37,13 @@ namespace FOGService.Tests.Core.Data
         public void RoundTrip_Protect()
         {
             // Roundtrip a message using DPAPI protection
-
             const string message = "The dog jumped over the fence #@//\\\\$";
             var messageBytes = Encoding.ASCII.GetBytes(message);
 
             var protectedBytes = DPAPI.ProtectData(messageBytes, true);
             var unProtectedBytes = DPAPI.UnProtectData(protectedBytes, true);
 
+            Assert.AreNotEqual(messageBytes, protectedBytes);
             Assert.AreEqual(messageBytes, unProtectedBytes);
         }
     }
