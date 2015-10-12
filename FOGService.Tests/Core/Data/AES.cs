c167ec0253ff482ed5db7bd07b53af1b17762ff0
Jbob <contact@jbob.io>
2015-10-12 11:08:03 -0500

    Remove redundant tests

--- FOGService.Tests/Core/Data/AES.cs
+++ FOGService.Tests/Core/Data/AES.cs
@@ -47,37 +47,5 @@ namespace FOGService.Tests.Core.Data
             Assert.AreEqual(length, pw2.Length);
             Assert.AreNotEqual(pw1, pw2);
         }
-
-        [Test]
-        public void RoundTrip_Base64()
-        {
-            /**
-            * Roundtrip a message by base64 encoding it then decoding it
-            */
-
-            const string message = "The dog jumped over the fence #@//\\\\$";
-
-            var encoded = Transform.EncodeBase64(message);
-            Assert.IsNotEmpty(encoded);
-            var decoded = Transform.DecodeBase64(encoded);
-
-            Assert.AreEqual(message, decoded);
-        }
-
-        [Test]
-        public void RoundTrip_HexByteString()
-        {
-            /**
-            * Roundtrip a hex string by converting it to a byte array and then back to a hex string
-            */
-
-            const string message = "bdb2ab3c401ef23602786e9caeb28266c18cbf06de4c634291eb4a0d51e5b7bb";
-
-            var encoded = Transform.HexStringToByteArray(message);
-            Assert.IsNotEmpty(encoded);
-            var decoded = Transform.ByteArrayToHexString(encoded);
-
-            Assert.AreEqual(message, decoded);
-        }
     }
 }
