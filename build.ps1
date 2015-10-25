11bb0d8f5554421565c4d6b4603448cf747354ee
Jbob <contact@jbob.io>
2015-10-24 19:17:28 -0500

    Build Tray with mono

--- build.ps1
+++ build.ps1
@@ -13,6 +13,8 @@ $buildMode = "/verbosity:minimal /p:configuration=Debug"
 $solutionConfig = "$PSScriptRoot\FOGService.sln $buildMode"
 $installerConfig = "$PSScriptRoot\UniversalInstaller\UniversalInstaller.csproj $buildMode"
 $msiConfig = "$PSScriptRoot\MSI\MSI.wixproj $buildMode"
+$xbuild = "xbuild.bat "
+$trayConfig = "$PSScriptRoot\Tray\Tray.csproj"
 
 Write-Host "Cleaning build directory"
 If (Test-Path "$PSScriptRoot\bin"){ Remove-Item -Recurse "$PSScriptRoot\bin" }
@@ -22,6 +24,9 @@ Invoke-Expression ($nuget + "restore")
 
 Write-Host "Building Solution"
 Invoke-Expression ($msbuild + $solutionConfig)
+
+Write-Host "Rebuilding Tray with mono"
+Invoke-Expression ($xbuild + $trayConfig)
 
 Write-Host "Copying Theme to build"
 Copy-Item "$PSScriptRoot\themes.xml" "$PSScriptRoot\bin\themes.xml"
