e2f7242cb16376b2fec1a9d6fc7a13a01f2e05f8
Jbob <contact@jbob.io>
2016-03-02 20:14:48 -0600

    Closes #31

--- SetupHelper/CustomAction.cs
+++ SetupHelper/CustomAction.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿/*
+ * FOG Service : A computer management client for the FOG Project
+ * Copyright (C) 2014-2016 FOG Project
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using FOG.Handlers.Data;
 using FOG.Handlers.Middleware;
 using Microsoft.Deployment.WindowsInstaller;
+using Microsoft.Win32;
 using Microsoft.Win32.TaskScheduler;
  
 namespace SetupHelper
@@ -138,6 +158,24 @@ namespace SetupHelper
 
         }
 
+        [CustomAction]
+        public static ActionResult CheckForLegacy(Session session)
+        {
+            try
+            {
+                var software =
+                    Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall").GetSubKeyNames();
+
+                if (software.Contains("{91C5D423-B6AB-4EAB-8F17-2BB3AE162CA1}"))
+                {
+                    DisplayMSIError(session, "Please uninstall the legacy client and re-run this installer");
+                    return ActionResult.Failure;
+                }
+            }
+            catch (Exception) { }
+
+            return ActionResult.Success;
+        }
 
         [CustomAction]
         public static ActionResult CleanTasks(Session session)
