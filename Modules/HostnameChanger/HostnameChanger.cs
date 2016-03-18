d2f92ee3211b8b313ce465095bbd319d15d1476a
Jbob <contact@jbob.io>
2016-03-18 15:20:31 -0500

    Revert to 0.9.11 domain code

--- Modules/HostnameChanger/HostnameChanger.cs
+++ Modules/HostnameChanger/HostnameChanger.cs
@@ -18,9 +18,15 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using FOG.Handlers;
 using FOG.Handlers.Middleware;
 using FOG.Handlers.Power;
+
 
 namespace FOG.Modules.HostnameChanger
 {
@@ -29,6 +35,43 @@ namespace FOG.Modules.HostnameChanger
     /// </summary>
     public class HostnameChanger : AbstractModule
     {
+        private readonly Dictionary<int, string> _returnCodes = new Dictionary<int, string>
+        {
+            {0, "Success"},
+            {2, "The OU parameter is not set properly or not working with this current setup"},
+            {5, "Access Denied"},
+            {87, "The parameter is incorrect"},
+            {110, "The system cannot open the specified object"},
+            {1326, "Logon failure: unknown username or bad password"},
+            {1355, "The specified domain either does not exist or could not be contacted"},
+            {2103, "The server could not be located"},
+            {2105, "A network resource shortage occured"},
+            {2691, "The machine is already joined to the domain"},
+            {2692, "The machine is not currently joined to a domain"}
+        };
+
+        [Flags]
+        private enum UnJoinOptions
+        {
+            NetsetupAccountDelete = 0x00000004
+        }
+
+        [Flags]
+        private enum JoinOptions
+        {
+            NetsetupJoinDomain = 0x00000001,
+            NetsetupAcctCreate = 0x00000002,
+        }
+
+        //Import dll methods
+        [DllImport("netapi32.dll", CharSet = CharSet.Unicode)]
+        private static extern int NetJoinDomain(string lpServer, string lpDomain, string lpAccountOU,
+            string lpAccount, string lpPassword, JoinOptions nameType);
+
+        [DllImport("netapi32.dll", CharSet = CharSet.Unicode)]
+        private static extern int NetUnjoinDomain(string lpServer, string lpAccount, string lpPassword,
+            UnJoinOptions fUnjoinOptions);
+
         public HostnameChanger()
         {
             Name = "HostnameChanger";
@@ -40,12 +83,11 @@ namespace FOG.Modules.HostnameChanger
             var taskResponse = Communication.GetResponse("/service/hostname.php?moduleid=" + Name.ToLower(), true);
 
             Log.Debug(Name, "AD Settings");
-            Log.Debug(Name, "   Hostname:  " + taskResponse.GetField("#hostname"));
-            Log.Debug(Name, "   AD:        " + taskResponse.GetField("#AD"));
-            Log.Debug(Name, "   ADDom:     " + taskResponse.GetField("#ADDom"));
-            Log.Debug(Name, "   ADOU:      " + taskResponse.GetField("#ADOU"));
-            Log.Debug(Name, "   ADUser:    " + taskResponse.GetField("#ADUser"));
-            Log.Debug(Name, "   Enforce:   " + taskResponse.GetField("#enforce"));
+            Log.Debug(Name, "   Hostname:" + taskResponse.GetField("#hostname"));
+            Log.Debug(Name, "   AD:" + taskResponse.GetField("#AD"));
+            Log.Debug(Name, "   ADDom:" + taskResponse.GetField("#ADDom"));
+            Log.Debug(Name, "   ADOU:" + taskResponse.GetField("#ADOU"));
+            Log.Debug(Name, "   ADUser:" + taskResponse.GetField("#ADUser"));
 
             if (taskResponse.Error) return;
 
@@ -53,11 +95,6 @@ namespace FOG.Modules.HostnameChanger
             {
                 Log.Error(Name, "Response was not encrypted");
                 return;
-            }
-
-            if (UserHandler.IsUserLoggedIn() && !taskResponse.GetField("#enforce").Equals("1"))
-            {
-                Log.Entry(Name, "Enforcement is disabled and a user is logged in, skipping module");
             }
 
             RenameComputer(taskResponse);
@@ -77,29 +114,29 @@ namespace FOG.Modules.HostnameChanger
                 Log.Error(Name, "Hostname is not specified");
                 return;
             }
-
             if (Environment.MachineName.ToLower().Equals(response.GetField("#hostname").ToLower()))
             {
                 Log.Entry(Name, "Hostname is correct");
                 return;
             }
-            
-            Log.Entry(Name, $"Renaming host to {response.GetField("#hostname")}");
 
+            Log.Entry(Name, string.Format("Renaming host to {0}", response.GetField("#hostname")));
+
+            Log.Entry(Name, "Unregistering computer");
+            //First unjoin it from active directory
             UnRegisterComputer(response);
-
             if (Power.ShuttingDown || Power.Requested) return;
-            UpdateHostnameRegistry(response.GetField("#hostname"));
+
+            Log.Entry(Name, "Updating registry");
+
+            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters", "NV Hostname",
+                response.GetField("#hostname"));
+            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName", "ComputerName",
+                response.GetField("#hostname"));
+            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName", "ComputerName",
+                response.GetField("#hostname"));
 
             Power.Restart(RegistryHandler.GetSystemSetting("Company") + " needs to rename your computer", Power.FormOption.Delay);
-        }
-
-        private void UpdateHostnameRegistry(string hostname)
-        {
-            Log.Entry(Name, "Updating registry, '");
-            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters", "NV Hostname", hostname);
-            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName", "ComputerName", hostname);
-            RegistryHandler.SetRegistryValue(@"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName", "ComputerName", hostname);
         }
 
         //Add a host to active directory
@@ -110,26 +147,65 @@ namespace FOG.Modules.HostnameChanger
 
             Log.Entry(Name, "Registering host with active directory");
 
-            if (!response.AreFieldsValid("#ADDom", "#ADUser", "#ADPass"))
+            if (!response.IsFieldValid("#ADDom") || !response.IsFieldValid("#ADUser") || !response.IsFieldValid("#ADPass"))
             {
                 Log.Error(Name, "Required Domain Joining information is missing");
                 return;
             }
 
-            if (WinDomain.InCorrectDomain(response))
+            // Check if the host is already part of the set domain by checking server IPs
+            try
             {
-                Log.Entry(Name, "Host is already joined to target domain");
-                return;
+                using (var domain = Domain.GetComputerDomain())
+                {
+                    var currentIP = Dns.GetHostAddresses(domain.Name);
+                    var targetIP = Dns.GetHostAddresses(response.GetField("#ADDom"));
+
+                    if (currentIP.Intersect(targetIP).Any())
+                    {
+                        Log.Entry(Name, "Host is already joined to target domain");
+                        return;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
             }
 
-            var returnCode = WinDomain.Join(response);
-            Log.Entry(Name, "Domain Join return code = " + returnCode);
 
-            if(WinDomain.ReturnCodes.ContainsKey(returnCode))
-                Log.Entry(Name, WinDomain.ReturnCodes[returnCode]);
+            // Attempt to join the domain
+            var returnCode = DomainWrapper(response, true, (JoinOptions.NetsetupJoinDomain | JoinOptions.NetsetupAcctCreate));
+
+            switch (returnCode)
+            {
+                case 2224:
+                    returnCode = DomainWrapper(response, true, JoinOptions.NetsetupJoinDomain);
+                    break;
+                case 2:
+                case 50:
+                case 1355:
+                    returnCode = DomainWrapper(response, false, (JoinOptions.NetsetupJoinDomain | JoinOptions.NetsetupAcctCreate));
+                    break;
+            }
+
+            // Entry the results
+            Log.Entry(Name,
+                           $"{(_returnCodes.ContainsKey(returnCode) ? $"{_returnCodes[returnCode]}, code = " : "Unknown Return Code: ")} {returnCode}");
 
             if (returnCode.Equals(0))
                 Power.Restart("Host joined to Active Directory, restart required", Power.FormOption.Delay);
+        }
+
+        private static int DomainWrapper(Response response, bool ou, JoinOptions options)
+        {
+            return NetJoinDomain(null,
+                response.GetField("#ADDom"),
+                ou ? response.GetField("#ADOU") : null,
+                response.GetField("#ADUser"),
+                response.GetField("#ADPass"),
+                options);
         }
 
         //Remove the host from active directory
@@ -137,20 +213,28 @@ namespace FOG.Modules.HostnameChanger
         {
             Log.Entry(Name, "Removing host from active directory");
 
-            if (!response.AreFieldsValid("#ADUser", "#ADPass"))
+            if (!response.IsFieldValid("#ADUser") || !response.IsFieldValid("#ADPass"))
             {
                 Log.Error(Name, "Required Domain information is missing");
                 return;
             }
 
-            var returnCode = WinDomain.Leave(response);
-            Log.Entry(Name, "Domain Leave return code = " + returnCode);
+            try
+            {
+                var returnCode = NetUnjoinDomain(null, response.GetField("#ADUser"),
+                    response.GetField("#ADPass"), UnJoinOptions.NetsetupAccountDelete);
 
-            if (WinDomain.ReturnCodes.ContainsKey(returnCode))
-                Log.Entry(Name, WinDomain.ReturnCodes[returnCode]);
+                Log.Entry(Name, string.Format("{0} {1}", (_returnCodes.ContainsKey(returnCode)
+                    ? string.Format("{0}, code = ", _returnCodes[returnCode])
+                    : "Unknown Return Code: "), returnCode));
 
-            if (returnCode.Equals(0))
-                Power.Restart("Host left active directory, restart needed", Power.FormOption.Delay);
+                if (returnCode.Equals(0))
+                    Power.Restart("Host left active directory, restart needed", Power.FormOption.Delay);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(Name, ex);
+            }
         }
 
         //Active a computer with a product key
