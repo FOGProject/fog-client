7d14396987335de2a989d3125f156467c4bdbc1c
Jbob <contact@jbob.io>
2016-06-23 19:35:45 -0500

    Increment to 0.11.2

--- UniversalInstaller/Helper.cs
+++ UniversalInstaller/Helper.cs
@@ -28,7 +28,7 @@ namespace FOG
     public static class Helper
     {
         private const string LogName = "Installer";
-        public const string ClientVersion = "0.11.1";
+        public const string ClientVersion = "0.11.2";
 
         public static IInstall Instance { get;  }
 
