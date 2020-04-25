af5dc810f03658a48d946756a8ff01a2d64c8999
Sebastian Roth <sebaroth@gmx.de>
2020-04-25 00:21:18 -0500

    Up version to 0.12.0

--- UniversalInstaller/Helper.cs
+++ UniversalInstaller/Helper.cs
@@ -28,7 +28,7 @@ namespace FOG
     public static class Helper
     {
         private const string LogName = "Installer";
-        public const string ClientVersion = "0.11.19";
+        public const string ClientVersion = "0.12.0";
 
         public static IInstall Instance { get;  }
 
